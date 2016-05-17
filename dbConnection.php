a69f253850ee4b4f1d55cb5785378912fd015948
Abeer Magdy <abeermagdy199525@gmail.com>
2016-05-17 15:23:41 +0200

    fixing

---
+++ dbConnection.php
@@ -0,0 +1,8 @@
+<?php
+define ('HOST','mysql8.000webhost.com');
+define('USER','a6271170_eman');
+define('PASS','1029384756db');
+define('DB','a6271170_ssApp');
+$con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+
+?>
