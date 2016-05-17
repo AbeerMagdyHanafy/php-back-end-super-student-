d5798ffbb49243f8808ec16c8a1faa4cf1471f06
Abeer Magdy <abeermagdy199525@gmail.com>
2016-05-17 15:03:33 +0200

    php

---
+++ registeration.php
@@ -0,0 +1,23 @@
+<?php
+if($_SERVER['REQUEST_METHOD']=='POST')
+{
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password=$_POST['password'];
+    require_once('dbConnect.php');
+    $sql="INSERT INTO volley (username,email,password) VALUES (' $username','$email','$password')";
+    if(mysqli_query($con,$sql))
+    {
+        echo"Successfully Registered";
+    }
+
+    else
+    {
+        echo "Could not register"
+    }
+}
+else
+{
+    echo 'Error'
+}
+?>
